968e77b01d2562a603d2bd03c617ab96eeb50f57
janayeager <164604754+janayeager@users.noreply.github.com>
2024-03-23 11:10:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def print_helloworld():
+  st.write("helloworld")
+
+def main():
+  # Call the print_helloworld function
+  print_helloworld()
+
+if __name__ == "__main__":
+  main()
